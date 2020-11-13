9b7b327c076bc199b4114f714390f502b9b68a79
Fabián Sánchez León <fadarsaleeing@gmail.com>
2020-11-13 12:18:38 -0500

    #2, testing header with style theme

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'ng-uui-header',
@@ -7,10 +7,71 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.Emulated
 })
 export class HeaderComponent implements OnInit {
-
+  @Input('appname') appname: any;
   constructor() { }
+
+  sidebarClases = {
+    open: false,
+    sidebarDivClase: 'sidebar_off',
+    sidebarContainerClase: 'main-container-sidebar-off',
+    containerDivClase: 'container-view',
+    containerBodyClase: 'container-body',
+    containerLogoCollapsedClase: 'inline-block',
+    containerLogoClase: 'none',
+    textoMenuLateralClase: 'menulateral-text'
+  };
+
+  notificacion = {
+    open: false,
+    clase: 'notificacion_container'
+  };
 
   ngOnInit(): void {
   }
 
+  logout() {
+    // token_service.logout();
+  }
+
+  toogleCerrarSesion() {
+    // var buttonCerrarSesion = document.getElementById('header-button-cerrarsesion-container');
+    // if (buttonCerrarSesion.style.display === 'none' || buttonCerrarSesion.style.display === '') {
+    //   buttonCerrarSesion.style.display = 'block';
+    // } else {
+    //   buttonCerrarSesion.style.display = 'none';
+    // }
+  }
+
+  toogleAplicaciones() {
+    // behaviorTheme.toogleAplicacion();
+  }
+
+  togglenotify() {
+    // if (!behaviorTheme.notificacion.open) {
+    //   notificacion.changeStateNoView();
+    // }
+    // behaviorTheme.toogleNotificacion();
+  }
+
+  sidebarEvent() {
+    // behaviorTheme.toogleSidebar();
+  }
+
 }
+
+// if (token_service.live_token()) {
+//   isLogin = true;
+//   notificacion = notificacion;
+//   token = token_service.getPayload();
+// } else {
+//   isLogin = false;
+// }
+
+// sidebarClases = behaviorTheme.sidebar;
+
+
+// var mediaquery = window.matchMedia('(max-width: 855px)');
+// if (mediaquery.matches) {
+//   behaviorTheme.toogleSidebar();
+//   behaviorTheme.toogleSidebar();
+// }
