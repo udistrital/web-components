56ce5f47e23e0952802c0d1dc681470c2b922151
Fabián Sánchez León <fadarsaleeing@gmail.com>
2020-11-13 10:16:51 -0500

    #2 feat: adding responsive and actionsicons

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -19,22 +19,27 @@ export class FooterComponent {
       list: [{
         title: 'Horario',
         class: 'time',
+        link: '',
         value: ['Lunes a viernes', '8am a 5pm']
       }, {
         title: 'Nombre',
         class: 'globe',
+        link: '',
         value: ['Sistema Integrado de informática y  Telecomunicaciones '],
       }, {
         title: 'Phone',
         class: 'call',
+        link: 'tel:0313239300',
         value: ['323 93 00', 'Ext. 1112'],
       }, {
         title: 'Direccion',
         class: 'pin',
+        link: 'https://goo.gl/maps/wURvmjEDR32YDe5z7',
         value: ['Cra 8 # 40-78', 'Piso 1']
       }, {
         title: 'mail',
         class: 'at',
+        link: 'mailto:computo@udistrital.edu.co',
         value: ['computo@udistrital.edu.co']
       }
       ],
